ed2b0a8efe2607e9f0dbe381bd080c252f125688
katy cmh <katy@cmh.tv>
2024-06-30 14:41:10 +0500

    add Task2

---
+++ Task2/Program.cs
@@ -0,0 +1,12 @@
+﻿//int n = 10;
+int [] array = {1, 2, 3, 4, 5, 6, 7, 8, 9,10};
+int i = 0;
+
+while (i < array.Length) 
+{
+    if (array[i] % 2 == 0)
+    {
+       Console.Write(array[i]);
+    }
+    i = i + 1;
+}
