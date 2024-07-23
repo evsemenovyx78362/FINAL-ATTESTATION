51b59675e87f664cb7e2ba3dec6b3b6580ed3af4
evsemenovyx78362 <160816908+evsemenovyx78362@users.noreply.github.com>
2024-07-23 11:25:27 +0500

    Update Program.cs

--- Task2/Program.cs
+++ Task2/Program.cs
@@ -59,31 +59,3 @@
 //         }
 //     }
 
-// }
-// int n = 10;
-// int [] array = {1, 2, 3, 4, 5, 6, 7, 8, 9,10};
-// int i = 0;
-
-// while (i < array.Length) 
-// {
-//     if (array[i] % 2 == 0)
-//     {
-//        Console.Write(array[i]);
-//     }
-//     i = i + 1;
-// }
-
-int n = 10;
-int [] array = {1, 2, 3, 4, 5, 6, 7, 8, 9,10};
-int i = 0;
-int max = array [0];
-
-while (i < n) 
-{
-    if (array[i] > max)
-    {
-       max = array[i];
-    }
-    i = i + 1;
-}
-Console.Write(max);
